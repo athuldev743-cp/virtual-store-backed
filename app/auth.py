6a219b1a1b47c15cb8e356377f0515d60c3c7045
athul dev <athuldev743@gmail.com>
2025-09-26 06:37:45 +0530

    ws

--- app/auth.py
+++ app/auth.py
@@ -21,7 +21,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 
 REFRESH_TOKEN_EXPIRE_DAYS: int = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
 
 # -------------------------------
-# Password hashing
+# Password hashin
 # -------------------------------
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
